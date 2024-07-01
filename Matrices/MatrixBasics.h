3a0e5fc708a4d23c4532e59b8560c854695eeb2f
SahasT23 <sahas.st01@gmail.com>
2024-07-01 18:47:09 +0100

    enough work for today

---
+++ Matrices/MatrixBasics.h
@@ -0,0 +1,23 @@
+#ifndef MATRIX2_H
+#define MATRIX2_H
+
+template <class T>
+class Matrix2
+{
+  public:
+
+  // Defining the appropriate constructors
+  
+  Matrix2();
+  Matrix2(int nRows, int nCols);
+  Matrix2(const Matrix2<T>& inputMatrix);
+
+  // Destructor
+
+  ~Matrix2();
+
+  // Configuration Methods
+
+  bool resize
+
+}
