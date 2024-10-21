e7f889ba95fa426cfef94946812b85961e89d747
SahasT23 <sahas.st01@gmail.com>
2024-10-21 08:45:51 +0100

    Added constructors

--- Matrices/MatrixBasics.h
+++ Matrices/MatrixBasics.h
@@ -44,6 +44,41 @@ class Matrix2
   template <class U> friend Matrix2<U> operator+ (const Matrix2<U>& lhs, const Matrix2<U>& rhs);
   template <class U> friend Matrix2<U> operator+ (const U& lhs, const Matrix2<U>& rhs);
   template <class U> friend Matrix2<U> operator+ (const Matrix2<U>& lhs, const U& rhs);
+
+  private:
+    int SubTwoInd(int row, int col);
+
+  private:
+    T *m_matrixData;
+    int m_nRows, m_nCols, m_nElements;
+
+// Default Constructor
+
+template <class T>
+Matrix2<T>::Matrix2()
+{
+  m_nRows = 1;
+  m_nCols = 1;
+  m_nElements = 1;
+  m_matrixData = new T[m_newElements];
+  m_matrixData[0] = 0.0;
 }
+
+// construct empty matrix (all empty elements)
+template <class T>
+Matrix2<T>::Matrix(int nRows, int nCols)
+{
+  m_nRows = nRows;
+  m_nCols = nCols;
+  m_nElements = m_nRows * m_nCols;
+  n_matrixData = new T[m_nElements];
+  for (int i=0; i<m_nElements; i++)
+    m_matrixData[i] = 0.0;
+}
+
+
+};
+
+
 
 // need to add more code, still doing research
